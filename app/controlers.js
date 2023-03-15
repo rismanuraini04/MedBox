699069650c0c0d1f59cfbfc6715911ec3e8adbcf
rismanuraini04 <rismanuraini04@gmail.com>
2023-03-15 21:37:17 +0700

    risma (fitur): menambahkan template engine

--- app/controlers.js
+++ app/controlers.js
@@ -1,3 +1,5 @@
+
+
 // Put your controller code here
 exports.login = (req, res) => {
     const data = {
@@ -6,12 +8,67 @@ exports.login = (req, res) => {
         layout: "layout/auth",
     };
     res.render("login", data);
+}; 
+
+exports.register = (req, res) => {
+    const data = {
+        styles: ["/style/signup.css"],
+        scripts: [],
+        layout: "layout/auth",
+    };
+    res.render("register", data);
 };
 
 exports.linkDevice = (req, res) => {
     const data = {
         styles: ["/style/page3.css"],
         scripts: [],
+        title: "Medicine Box",
     };
     res.render("page3", data);
 };
+
+exports.dashboard = (req, res) => {
+    const data = {
+        styles: ["/style/page4.css"],
+        scripts: [],
+    };
+    res.render("page4", data);
+}
+
+exports.pageOptions = (req, res) => {
+    const id = req.params.id
+    const data = {
+        styles: ["/style/page5.css"],
+        scripts: [],
+        title: `Box ${id} options`,
+    };
+    res.render("page5", data);
+}
+
+exports.scheduleReminder = (req, res) => {
+    const data = {
+        styles: ["/style/page8.css", "/style/page6.css"],
+        scripts: [],
+        title: "Box 1 Schedule Reminder",
+    };
+    res.render("page6", data);
+}
+
+exports.profile = (req, res) => {
+    const data = {
+        styles: ["/style/page2.css"],
+        scripts: [],
+        layout: "layout/profile",
+    };
+    res.render("page2", data);
+};
+
+exports.history = (req, res) => {
+    const data = {
+        styles: ["/style/page12.css"],
+        scripts: [],
+        title: "Box 1 History",
+    };
+    res.render("page12", data);
+}
--- app/router.js
+++ app/router.js
@@ -1,7 +1,13 @@
 const router = require("express").Router();
 const controller = require("./controlers");
 
-router.get("/", controller.linkDevice);
+router.get("/", controller.dashboard);
+router.get("/link-device", controller.linkDevice);
 router.get("/login", controller.login);
+router.get("/register", controller.register);
+router.get("/options/:id", controller.pageOptions);
+router.get("/schedule", controller.scheduleReminder);
+router.get("/profile", controller.profile);
+router.get("/history", controller.history);
 
 module.exports = router;
